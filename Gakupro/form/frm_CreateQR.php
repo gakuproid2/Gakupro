7ad4488fe178be046414e625989531a989f47f85
gakuproid2 <90078924+gakuproid2@users.noreply.github.com>
2021-10-22 21:08:32 +0900

    Add files via upload

---
+++ Gakupro/form/frm_CreateQR.php
@@ -0,0 +1,356 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<?php
+
+session_start(); //セッションスタート
+
+//クラスファイルの読み込み
+require_once '../php/common.php';
+//クラスの生成
+$common = new common();
+
+$HeaderInfo = $common->HeaderCreation();
+
+$JS_Info = $common->Read_JSconnection();
+
+?>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/Header.css">
+  <title>QRコード作成画面</title>
+
+  <div class='Header'>
+    <?php echo $HeaderInfo; ?>
+    <div class='Header_ScreenName'>
+      <p>QRコード作成画面</p>
+    </div>
+  </div>
+</head>
+
+<?php
+
+$alert = "";
+
+if (!empty($_POST["Request"])) {
+
+  //クラスファイルの読み込み
+  require_once '../dao/dao_CreateQR.php';
+  //クラスの生成
+  $dao = new dao_CreateQR();
+
+  // QRcode作成用「QrCodeHelper.php」の読み込み
+  require("../php/QRCode/Create_QRCode.php");
+  // インスタンスの作成
+  $qrcode = new QrCodeHelper;
+
+  //前画面で要求された作成数を格納
+  $Request = $_POST["Request"];
+
+  //前画面で要求された日付を格納
+  $Date = str_replace("-", "", $_POST["TargetDate"]);
+
+  //日付で現状の作成数を取得
+  $Quantity = $dao->Get_Quantity($Date);
+
+  //リクエスト数より作成数が上回っているはアラートを表示
+  //リクエスト数が上回っている場合は差分を作成
+  if ($Request <= $Quantity) {
+
+    $alert = "<h2>選択日(" . $Date . ")では既に[" . $Quantity . "]個のQRコードが作成されています。</h2>";
+  } else {
+
+    //基本のディリクトリ
+    $Base_Dir = '../File/';
+
+    //基本ディリクトリに日付ディリクトリを作成する為の文字列格納
+    $Date_Dir = $Base_Dir . $Date . '/';
+
+    //基本ディリクトリ + 日付ディリクトリの存在チェック
+    if (!file_exists($Date_Dir)) {
+      //存在しないときはフォルダ作成
+      mkDir($Date_Dir, 0777);
+    }
+
+    //日付ディリクトリにQRディリクトリを作成する為に文字列格納
+    $QR_Dir = $Date_Dir . 'QR/';
+
+    //日付ディリクトリ + QRディリクトリの存在チェック
+    if (!file_exists($QR_Dir)) {
+      //存在しないときはフォルダ作成    
+      mkDir($QR_Dir, 0777);
+    }
+
+    //--パスワード作成処理--開始--
+    //パスワード格納用配列宣言
+    $Password_array = [];
+
+    //日付で既存データのパスワードを取得
+    $Password_Table = $dao->Get_PassWord($Date);
+
+    //パスワードを取得した分パスワード格納用配列に追加
+    foreach ($Password_Table as $Pass_val) {
+      array_push($Password_array, $Pass_val);
+    }
+
+    //パスワード格納用配列のデータ数を格納
+    $cnt = count($Password_array);
+
+    // 乱数の範囲(4桁に設定する)
+    $Password_min = 1000;
+    $Password_max = 9999;
+
+    //要求された作成数になるようループ
+    for ($i = $cnt; $i <= $Request; $i++) {
+
+      while (true) {
+
+        //設定した最小値と最大値の範囲内で乱数生成
+        $Password_tmp = mt_rand($Password_min, $Password_max);
+
+        //パスワード格納用配列と比較し重複していなければ配列に格納
+        if (!in_array($Password_tmp, $Password_array)) {
+          array_push($Password_array, $Password_tmp);
+          break;
+        }
+      }
+    }
+
+    //作成したパスワードを登録(要求された作成数分ループ)
+    for ($i = $cnt + 1; $i <= $Request; $i++) {
+
+      //キーコードは、日付(yyyymmdd) + 連番(0埋め3桁)
+      $Key_Code = $Date  . str_pad($i, 3, 0, STR_PAD_LEFT);
+
+      //キーコードとパスワードを登録する
+      $Result = $dao->Insert_ImageGet_T($Key_Code, $Password_array[$i]);
+    }
+
+    //後ほど削除予定↓
+    //別端末での検証時はURLがlocalhostでは画面遷移できない。
+    //PCのIPアドレスをURLにする必要がある。
+    $HostName = '';
+    if (empty($_POST["HostName"])) {
+      $HostName = 'http://localhost';
+    } else {
+      $HostName = 'http://' . $_POST["HostName"];
+    }
+    //後ほど削除予定↑
+
+
+    //QRcodeチケットのテンプレ保存場所
+    $Template_Dir = $Base_Dir . 'Template/QR_Template.png';
+
+    //QRcodeチケットの保存場所
+    $QrTicket_Dir = $Date_Dir . 'QR_Ticket/';
+
+    if (!file_exists($QrTicket_Dir)) {
+      //存在しないときはフォルダ作成
+      mkDir($QrTicket_Dir, 0777);
+    }
+
+    //日付でデータ（Key_CodeとPassWord）取得
+    $Key_Code_Table = $dao->Get_ImageGet_T($Date);
+
+    //データ数分のQRチケットを作成
+    foreach ($Key_Code_Table as $value) {
+
+      //キーコード格納
+      $Key_Code = $value['Key_Code'];
+      //パスワード格納
+      $PassWord = $value['PassWord'];
+      //キーコードから日付を除去しコード作成格納　例：20201231001 - 20201231 → 001
+      $Code = str_replace($Date, "", $Key_Code);
+
+      //QRコードの作成から保存まで--開始--
+
+      // QRコードに設定するURLの指定
+      $url = $HostName . "/gakupro/form/ScreenSelectionBranch.php?Key_Code=" . $Key_Code;
+
+      //QRコード作成処理(ネットで見つけた処理、設定したい$urlを以下の処理に渡すと作成してくれる)
+      $TargetUrl =  $qrcode->text($url, ["size" => "150x150", "encode" => "UTF-8", "margin" => "2", "error_correction" => "L"]) . '/chart.png';
+
+      //作成されたQRを取得  
+      $img = file_get_contents($TargetUrl);
+
+      //作成したQRの保存名を設定
+      $CreateQR_Dir = $QR_Dir . $Key_Code . '.png';
+
+      //画像保存時、$QR_Dir(保存場所) + $Key_Code(日付と連番を連結)で保存
+      file_put_contents($CreateQR_Dir, $img);
+
+      //QRコードの作成から保存まで--終了--
+
+      // Template画像を取り込み新しくQRチケットを作成する為
+      $Create_img = imagecreatefrompng($Template_Dir);
+
+      // 挿入する文字列のフォント(今回はWindowsに入ってたメイリオを使う)   
+      /////Macで作成できるか確認必須///// 
+      $font = "../Fonts/meiryo.ttc";
+
+      //日付挿入--開始--
+      // 挿入する文字列
+      $text = substr($Date, 0, 4) . '/' . substr($Date, 4, 2) . '/' . substr($Date, 6, 2);
+
+      // 挿入する文字列の色(白)
+      $color = imagecolorallocate($Create_img, 1, 5, 12);
+
+      // 挿入する文字列のサイズ(ピクセル)
+      $size = 13;
+
+      // 挿入する文字列の角度
+      $angle = 0;
+
+      // 挿入位置
+      $x = 5;         // 左からの座標(ピクセル)
+      $y = imagesy($Create_img) - 30; // 上からの座標(ピクセル)
+
+      // 文字列挿入
+      imagettftext(
+        $Create_img,     // 挿入先の画像
+        $size,      // フォントサイズ
+        $angle,     // 文字の角度
+        $x,         // 挿入位置 x 座標
+        $y,         // 挿入位置 y 座標
+        $color,     // 文字の色
+        $font,  // フォントファイル
+        $text
+      );     // 挿入文字列
+      //日付挿入--終了--
+
+
+      //キーナンバー挿入--開始--
+      // 挿入する文字列
+      $text = 'No.' . str_pad($Code, 3, 0, STR_PAD_LEFT);
+
+      // 挿入する文字列の色(白)
+      $color = imagecolorallocate($Create_img, 1, 5, 12);
+
+      // 挿入する文字列のサイズ(ピクセル)
+      $size = 13;
+
+      // 挿入する文字列の角度
+      $angle = 0;
+
+      // 挿入位置
+      $x = imagesx($Create_img) - 65; // 左からの座標(ピクセル)
+      $y = imagesy($Create_img) - 30; // 上からの座標(ピクセル)
+
+      // 文字列挿入
+      imagettftext(
+        $Create_img,     // 挿入先の画像
+        $size,      // フォントサイズ
+        $angle,     // 文字の角度
+        $x,         // 挿入位置 x 座標
+        $y,         // 挿入位置 y 座標
+        $color,     // 文字の色
+        $font,  // フォントファイル
+        $text
+      );     // 挿入文字列
+
+      //キーナンバー挿入--終了--
+
+      //パスワード挿入--開始--
+
+      // 挿入する文字列
+      $text = 'Pass :' . $PassWord;
+
+      // 挿入する文字列の色(白)
+      $color = imagecolorallocate($Create_img, 1, 5, 12);
+
+      // 挿入する文字列のサイズ(ピクセル)
+      $size = 13;
+
+      // 挿入する文字列の角度
+      $angle = 0;
+
+      // 挿入位置
+      $x = imagesx($Create_img) * 0.3; // 左からの座標(ピクセル)
+      $y = imagesy($Create_img) - 5; // 上からの座標(ピクセル)
+
+      // 文字列挿入
+      imagettftext(
+        $Create_img,     // 挿入先の画像
+        $size,      // フォントサイズ
+        $angle,     // 文字の角度
+        $x,         // 挿入位置 x 座標
+        $y,         // 挿入位置 y 座標
+        $color,     // 文字の色
+        $font,  // フォントファイル
+        $text
+      );     // 挿入文字列
+
+      //パスワード挿入--終了--
+
+      //テンプレート画像とQRの合成--開始--
+
+      //QRコードの余白を除去する為の土台
+      $ReCreate_Qr_img = imagecreatetruecolor(111, 111);
+
+      //余白を除去する画像(QRコード)を取り込む
+      $Qr_img = imagecreatefrompng($CreateQR_Dir);
+
+      //余白除去処理→除去済み画像($ReCreate_Qr_img)
+      imagecopyresampled($ReCreate_Qr_img, $Qr_img, 0, 0, 19, 19, 111, 111, 111, 111);
+
+      // 合成する画像のサイズを取得
+      $sx = imagesx($ReCreate_Qr_img);
+      $sy = imagesy($ReCreate_Qr_img);
+
+      imageLayerEffect($ReCreate_Qr_img, IMG_EFFECT_ALPHABLEND); // 合成する際、透過を考慮する
+      imagecopy($Create_img, $ReCreate_Qr_img, 65, 95, 0, 0, $sx, $sy); // 合成する
+
+      imagepng($Create_img, $QrTicket_Dir . "QR_Ticket(" . $Key_Code . ").png");
+      imagedestroy($Create_img);
+
+      //テンプレート画像とQRの合成--終了--
+
+    }
+  }
+}
+?>
+
+<body>
+
+  <?php echo $alert; ?>
+  <form action="frm_CreateQR.php" method="post">
+    <p>出動日<input type="date" id="txt_TargetDate" name="TargetDate" autocomplete="off" maxlength="8"></p>
+
+    <p>作成数<input type="text" id="txt_Request" name="Request" autocomplete="off"></p>
+    <br>
+    <p>※※他端末で検証する場合のみホスト名、またはIPアドレスを入力してください。※※</p>
+    <p>ホスト名<input type="text" id="txt_HostName" name="HostName" autocomplete="off"></p>
+    <button class="btn_Create" id="btn_Create" name="Create">作成</button>
+  </form>
+
+  <?php echo $JS_Info ?>
+</body>
+
+<script>
+  //作成ボタンクリック時
+  $('#btn_Create').on('click', function() {
+
+    var ErrorMsg = '';
+    if ($("#txt_TargetDate").val() == "") {
+      ErrorMsg += '出動日が選択してください。\n';
+    }
+
+    if ($("#txt_Request").val() == "") {
+      ErrorMsg += '作成数を入力してください。\n';
+    }
+
+    if (!ErrorMsg == "") {
+      ErrorMsg = '以下は必須項目です。\n' + ErrorMsg;
+      window.alert(ErrorMsg);
+      return false;
+    } else {
+      $('#form_id').submit();
+    }
+
+  });
+</script>
+
+</html>
---
+++ Gakupro/form/frm_School_M.php
@@ -0,0 +1,311 @@
+<!DOCTYPE html>
+<html lang="ja">
+
+<?php
+  session_start(); //セッションスタート
+
+  //クラスファイルの読み込み
+  require_once '../php/common.php';
+  //クラスの生成
+  $common = new common();
+  
+  $HeaderInfo = $common->HeaderCreation();  
+
+  $JS_Info = $common->Read_JSconnection();
+?>
+
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../css/style.css">
+  <link rel="stylesheet" href="../css/Header.css">
+  <title>学校マスタ</title>
+
+  <div class ='Header'>
+    <?php echo $HeaderInfo; ?>
+    <div class ='Header_ScreenName'><p>学校マスタ画面</p></div>
+  </div>
+</head>
+
+<body>
+
+  <?php
+    $CD = 0;
+    //学校区分があれば格納する
+    if (!empty($_GET['School_Division'])) {
+      //if ($_GET['chk_Search'] == 1) {
+        $CD = $_GET['School_Division'];
+      //}
+    } //else {
+      //$CD = 0;
+    //}
+
+    $check = "";
+    //チェックボックスの０か１を格納する
+    if (!empty($_GET['chk_Search'])) {
+      if (!$_GET['chk_Search'] == 0) {
+        $check = 'checked="checked"';
+      } else {
+        $check = "";
+      }
+    }
+
+    //クラスファイルの読み込み
+    require_once '../dao/dao_School_M.php';
+    //クラスの生成
+    $dao = new dao_School_M();
+
+    //ポストされた確認する。
+    if (count($_POST) > 0) {
+
+      $UsageFlag = 0;
+      if (isset ($_POST["UsageFlag"])){
+        $UsageFlag = 1;
+      };
+      
+      $info = array(
+        'CD' => $_POST["CD"],
+        'Division' => $_POST["Division"],
+        'Name' => $_POST["Name"],
+        'TEL' => $_POST["TEL"],
+        'URL' => $_POST["URL"],
+        'UsageFlag' => $UsageFlag,
+        'Changer' => $_SESSION["Staff_ID"],
+        'UpdateDate' => date("Y-m-d H:i:s")
+      );
+      
+      $Result = "";
+      
+      //登録、削除、更新の分岐
+      if (isset($_POST['Insert'])) {
+        $Result = $dao->DataChange($info, 1);
+      } else if (isset($_POST['Update'])) {
+        $Result = $dao->DataChange($info, 2);
+      } else if (isset($_POST['Delete'])) {
+        $Result = $dao->DataChange($info, 3);
+      }
+
+      Header('Location: ' . $_SERVER['PHP_SELF']);
+      exit(); //optional
+    }
+
+    //学校区分のプルダウン作成する為
+    $items = $dao->GET_Subcategory_m();
+    //0行目
+    $PullDown = "<option value = 0 >選択してください</option>";
+    foreach ($items as $item_val) {
+
+      $PullDown .= "<option value = " . $item_val['SubCategory_CD'];
+
+      if ($CD == $item_val['SubCategory_CD']) {
+        $PullDown .= " selected>";
+      } else {
+        $PullDown .= " >";
+      }
+      $PullDown  .= $item_val['SubCategory_Name'] . "</option>";
+    }
+    
+    //School_MのMaxCD取得処理
+    $Max_CD = $dao->Get_MaxCD();
+    
+    $Data_Table = $dao->Get_School_M($CD, $check);
+    $Table = "";
+    
+    foreach ($Data_Table as $val) {
+      $Table .= "<tr class='Table'>
+      <td>" . $val['School_CD'] . "</td>
+      <td>" . $val['School_Division'] . "</td>
+      <td>" . $val['School_Name']." </td>
+      <td>" . $val['TEL']." </td>
+      <td>" . $val['URL']." </td>";
+    
+      if ($val['UsageFlag'] == 0) {
+        $Table .=" <td>×</td>";
+      } else {
+        $Table .=" <td>〇</td>";
+      }
+      
+      
+      $Table .= "</tr>";
+    }
+
+  ?>
+
+  <form action="frm_School_M.php" method="post">
+    <p>学校CD：<input type="text" id="txt_CD" name="CD" value='<?php echo $Max_CD; ?>' readonly> </p>
+    <p>
+      学校区分：<select id='School_Division' name='Division'><?php echo $PullDown; ?></select>　
+      データ絞り込み機能：<input type="checkbox" id="chk_Search" name="Search" value="1" <?php echo $check; ?>>
+    </p>
+    <p>学校名：<input type="text" id="txt_Name" name="Name" autocomplete="off"></p>
+    <p>代表電話番号：<input type="text" id="txt_TEL" name="TEL" autocomplete="off"></p>
+    <p>ホームページURL：<input type="text" id="txt_URL" name="URL" autocomplete="off"></p>
+    <p>利用フラグ：<input type="checkbox" id="chk_UsageFlag" name="UsageFlag" value="1" checked="checked"></p>
+
+    <button class="btn_Insert" id="btn_Insert" name="Insert" value="1">登録</button>
+    <button class="btn_Update" id="btn_Update" name="Update" value="2">更新</button>
+    <button class="btn_Delete" id="btn_Delete" name="Delete" value="3">削除</button>
+    <button class="btn_Clear" id="btn_Clear" name="Clear" value="4">クリア</button>
+  </form>
+
+  <table border='1'>
+    <tr>
+      <th>学校CD</th>
+      <th>学校区分</th>
+      <th>学校名</th>
+      <th>代表電話番号</th>
+      <th>ホームページURL</th>
+      <th>利用フラグ</th>
+    </tr>
+    <?php echo $Table; ?>
+  </table>
+
+  <?php echo $JS_Info?>
+</body>
+
+<script>
+  $(window).on('load', function(event) {
+    $("#btn_Insert").show();
+    $("#btn_Update").hide();
+    $("#btn_Delete").hide();
+  });
+
+  $('.Table').on('click', function() {
+      //学校CD
+      var School_CD = $(this).children('td')[0].innerText;
+      $("#txt_CD").val(School_CD);
+
+      //学校区分
+      var School_Division = $(this).children('td')[1].innerText;
+      $("#School_Division").val(School_Division);
+
+      //学校名
+      var School_Name = $(this).children('td')[2].innerText;
+      $("#txt_Name").val(School_Name);
+
+      //代表電話番号
+      var TEL = $(this).children('td')[3].innerText;
+      $("#txt_TEL").val(TEL);
+
+      //ホームページURL
+      var URL = $(this).children('td')[4].innerText;
+      $("#txt_URL").val(URL);
+
+      //利用フラグ
+      var UsageFlag = $(this).children('td')[5].innerText;
+
+    if (UsageFlag == '〇') {
+      $("#chk_UsageFlag").prop('checked', true);
+    } else {
+      $("#chk_UsageFlag").prop('checked', false);
+    }
+
+    $("#btn_Insert").hide();
+    $("#btn_Update").show();
+    $("#btn_Delete").show();
+  });
+
+  //チェックボックスでデータを絞り込む機能を作成
+  document.getElementById("chk_Search").onchange = function() {
+    const Division = document.getElementById("School_Division");
+    const value = Division.value;
+    var check;
+    <?php 
+    if ($check != "") {
+    ?>
+    check = 0;
+    <?php
+    } else {
+    ?>
+    check = 1;
+    <?php
+    }
+    ?>
+    window.location.href = 'frm_School_M.php?School_Division=' + value + '&chk_Search=' + check; // 通常の遷移
+  };
+
+  //チェックボックスがON状態の場合
+  <?php
+  if (!empty($_GET['chk_Search'])) {
+    if ($_GET['chk_Search'] == 1) {
+  ?>
+  document.getElementById("School_Division").onchange = function() {
+    var CD = this.value;
+    var check = 1;
+    window.location.href = 'frm_School_M.php?School_Division=' + CD + '&chk_Search=' + check; // 通常の遷移
+  }
+  <?php
+    }
+  }
+  ?>
+
+  //登録ボタンクリック時
+  $('#btn_Insert').on('click', function() {
+    if (ValueCheck() == false) {
+      return false;
+    }
+
+    if (window.confirm('登録してもよろしいですか？')) {
+      $('#form_id').submit();
+    } else {
+      return false;
+    }
+  });
+
+  //更新ボタンクリック時
+  $('#btn_Update').on('click', function() {
+    if (ValueCheck() == false) {
+      return false;
+    }
+
+    if (window.confirm('更新してもよろしいですか？')) {
+      $('#form_id').submit();
+    } else {
+      return false;
+    }
+  });
+
+  //削除ボタンクリック時
+  $('#btn_Delete').on('click', function() {
+    if (window.confirm('削除してもよろしいですか？')) {
+      $('#form_id').submit();
+    } else {
+      return false;
+    }
+  });
+
+  //クリアボタンクリック時
+  $('#btn_Clear').on('click', function() {
+    window.location.href = 'frm_MainCategory_M.php'
+  });
+
+  function ValueCheck() {
+
+    var ErrorMsg = '';
+    if ($("#School_Division").val() == "") {
+      ErrorMsg += '学校区分を選択してください。\n';
+    }
+
+    if ($("#txt_Name").val() == "") {
+      ErrorMsg += '学校名を入力してください。\n';
+    }
+
+    if ($("#txt_TEL").val() == "") {
+      ErrorMsg += '代表電話番号を入力してください。\n';
+    }
+
+    if ($("#txt_URL").val() == "") {
+      ErrorMsg += 'ホームページURLを入力してください。\n';
+    }
+
+    if (!ErrorMsg == "") {
+      ErrorMsg = '以下は必須項目です。\n' + ErrorMsg;
+      window.alert(ErrorMsg); // 
+      return false;
+    } else {
+      return true;
+    }
+  }
+</script>
+
+</html>
