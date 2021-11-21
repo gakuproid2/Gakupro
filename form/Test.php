aa247fc2fdc75b17f1c222836b1f46589b9db77a
gakuproid1 <87515343+gakuproid1@users.noreply.github.com>
2021-11-21 22:58:24 +0900

    a

--- form/Test.php
+++ form/Test.php
@@ -1,31 +1,32 @@
 <!doctype html>
 <html lang="ja">
+
 <head>
-  <meta charset="UTF-8">  
+  <meta charset="UTF-8">
   <title>テスト画面</title>
 </head>
 
 <?php
-  session_start(); //セッションスタート
+session_start(); //セッションスタート
 
-  //クラスファイルの読み込み
-  require_once '../php/common.php';
-  //クラスの生成
-  $common = new common();
+//クラスファイルの読み込み
+require_once '../php/common.php';
+//クラスの生成
+$common = new common();
 
-  //クラスファイルの読み込み
-  require_once '../dao/dao_Staff_M.php';
-  //クラスの生成
-  $dao_Staff_M = new dao_Staff_M();
+//クラスファイルの読み込み
+require_once '../dao/dao_Staff_M.php';
+//クラスの生成
+$dao_Staff_M = new dao_Staff_M();
 
-  //クラスファイルの読み込み
-  require_once '../dao/dao_SubCategory_M.php';
-  //クラスの生成
-  $dao_SubCategory_M = new dao_SubCategory_M();
-  
-  $HeaderInfo = $common->HeaderCreation('テスト画面');  
+//クラスファイルの読み込み
+require_once '../dao/dao_SubCategory_M.php';
+//クラスの生成
+$dao_SubCategory_M = new dao_SubCategory_M();
 
-  $JS_Info = $common->Read_JSconnection();
+$HeaderInfo = $common->HeaderCreation('テスト画面');
+
+$JS_Info = $common->Read_JSconnection();
 ?>
 
 <?php echo $HeaderInfo; ?>
@@ -37,50 +38,57 @@
 <body>
 
 
-<!-- Button trigger modal -->
-<button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal">
-  Launch demo modal
-</button>
+  <!-- Button trigger modal -->
+  <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#exampleModal" data-id="7777">
+    モーダルテスト
+  </button>
 
-<!-- Modal -->
-<div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
-  <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
-    <div class="modal-content">
-      <div class="modal-header">
-        <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
-        <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
-      </div>
-      <div class="modal-body">
-          
-        <div class="form-group row">
-              <label for="place_id" class="col-md-3 col-form-label">A:</label>
-              <input type="text" name="place_id" id="place_id" value="" class="form-control col-md-3" readonly>
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
         </div>
-        
-        <div class="form-group row">
-              <label for="place_id" class="col-md-3 col-form-label">B:</label>
-              <input type="text" name="place_id" id="place_id" value="" class="form-control col-md-3" readonly>
-        </div>
-        
+        <div class="modal-body">
 
-      <div class="modal-footer">
-        <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
-        <button type="button" class="btn btn-primary">Save changes</button>
+          <div class="form-group row">
+            <label for="place_id" class="col-md-3 col-form-label">A:</label>
+            <input type="text" name="place_id" id="place_id" value="" class="form-control col-md-3" readonly>
+          </div>
+
+          <div class="form-group row">
+            <label for="place_id" class="col-md-3 col-form-label">B:</label>
+            <input type="text" name="place_id" id="place_id" value="" class="form-control col-md-3" readonly>
+          </div>
+
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
       </div>
     </div>
-  </div>
-</div>
 
 
 
 
-  <?php echo $JS_Info?>
+    <?php echo $JS_Info ?>
 </body>
 
 <script>
+  $('#exampleModal').on('show.bs.modal', function(e) {
 
 
+    $('#place_id').val(1324);
 
+    // イベント発生元
+    let evCon = $(e.relatedTarget);
+    $('#place_id').val(evCon.data('id'));
+
+  });
 </script>
 
 
--- form/frm_MainCategory_M.php
+++ form/frm_MainCategory_M.php
@@ -1,83 +1,89 @@
-<!DOCTYPE html>
+!DOCTYPE html>
 <html lang="ja">
 
 <?php
-  session_start(); //セッションスタート
+session_start(); //セッションスタート
 
-  //クラスファイルの読み込み
-  require_once '../php/common.php';
-  //クラスの生成
-  $common = new common();
-  
-  //クラスファイルの読み込み
-  require_once '../dao/dao_MainCategory_M.php';
-  //クラスの生成
-  $dao_MainCategory_M = new dao_MainCategory_M();
-  
-  $HeaderInfo = $common->HeaderCreation(4);  
+//クラスファイルの読み込み
+require_once '../php/common.php';
+//クラスの生成
+$common = new common();
 
-  $JS_Info = $common->Read_JSconnection();
+//クラスファイルの読み込み
+require_once '../dao/dao_MainCategory_M.php';
+//クラスの生成
+$dao_MainCategory_M = new dao_MainCategory_M();
+
+$HeaderInfo = $common->HeaderCreation(4);
+
+$JS_Info = $common->Read_JSconnection();
 ?>
 
 <?php echo $HeaderInfo; ?>
 
 <?php
- 
-  
 
-  //ポストされた確認する。
-  if (count($_POST) > 1) {
 
-    $UsageFlag = 0;
-    if (isset($_POST["UsageFlag"])) {
-      $UsageFlag = 1;
-    };
 
-    $info = array(
-      'MainCategory_CD' => $_POST["MainCategory_CD"],
-      'MainCategory_Name' => $_POST["MainCategory_Name"],
-      'UsageFlag' => $UsageFlag,
-      'Changer' => $_SESSION["Staff_ID"],
-      'UpdateDate' => date("Y-m-d H:i:s")
-    );
+//ポストされた確認する。
+if (count($_POST) > 1) {
 
-    $Result = "";
+  $UsageFlag = 0;
+  if (isset($_POST["UsageFlag"])) {
+    $UsageFlag = 1;
+  };
 
-    //登録、削除、更新の分岐
-    if (isset($_POST['Insert'])) {
-      $Result = $dao_MainCategory_M->DataChange($info, 1);
-    } else if (isset($_POST['Update'])) {
-      $Result = $dao_MainCategory_M->DataChange($info, 2);
-    } else if (isset($_POST['Delete'])) {
-      $Result = $dao_MainCategory_M->DataChange($info, 3);
-    }
+  $info = array(
+    'MainCategory_CD' => $_POST["MainCategory_CD"],
+    'MainCategory_Name' => $_POST["MainCategory_Name"],
+    'UsageFlag' => $UsageFlag,
+    'Changer' => $_SESSION["Staff_ID"],
+    'UpdateDate' => date("Y-m-d H:i:s")
+  );
 
-    Header('Location: ' . $_SERVER['PHP_SELF']);
-    exit(); //optional
+  $Result = "";
+
+  //登録、削除、更新の分岐
+  if (isset($_POST['Insert'])) {
+    $Result = $dao_MainCategory_M->DataChange($info, 1);
+  } else if (isset($_POST['Update'])) {
+    $Result = $dao_MainCategory_M->DataChange($info, 2);
+  } else if (isset($_POST['Delete'])) {
+    $Result = $dao_MainCategory_M->DataChange($info, 3);
   }
 
-  //MainCategory_MのMaxCD取得処理
-  $Max_CD = $dao_MainCategory_M->Get_MaxCD();
+  Header('Location: ' . $_SERVER['PHP_SELF']);
+  exit(); //optional
+}
 
-  $Data_Table = $dao_MainCategory_M->Get_MainCategory_M();
+//MainCategory_MのMaxCD取得処理
+$Max_CD = $dao_MainCategory_M->Get_MaxCD();
 
-  $Table = "";
-  foreach ($Data_Table as $val) {
+$Data_Table = $dao_MainCategory_M->Get_MainCategory_M();
 
-    $Table .=
+$Table = "
+<table border='1'>
+<tr>
+  <th>大分類コード</th><th>大分類名</th><th>ボタン</th>
+</tr>
+";
+foreach ($Data_Table as $val) {
+
+  $Table .=
     "<tr class='Table'>
-    <td>" . $val['MainCategory_CD'] . "</td>
-    <td>" . $val['MainCategory_Name'] . " </td>";
+  <td>" . $val['MainCategory_CD'] . "</td>
+  <td>" . $val['MainCategory_Name'] . " </td>
+  <td>
+    <button type='button' class='' data-bs-toggle='modal' data-bs-target='#exampleModal' 
+    data-MainCategory_CD='" . $val['MainCategory_CD'] . "'
+    data-MainCategory_Name='" . $val['MainCategory_Name'] . "'
+    data-UsageFlag='" . $val['UsageFlag'] . "'>モーダルテスト</button>
+  </td>";
+}
+$Table .= "</table>";
 
-    if ($val['UsageFlag'] == 0) {
-      $Table .= " <td>×</td>";
-    } else {
-      $Table .= " <td>〇</td>";
-    }
-
-    $Table .= "</tr>";
-  }
 ?>
+
 <body>
 
   <form action="frm_MainCategory_M.php" method="post">
@@ -91,48 +97,84 @@
     <button class="btn_Clear" id="btn_Clear" name="Clear" value="4">クリア</button>
   </form>
 
-  <table border='1'>
-    <tr>
-    <th>大分類コード</th>
-    <th>大分類名</th>
-    <th>利用フラグ</th>
-    </tr>
-    <?php echo $Table; ?>
-  </table>
+  <?php echo $Table; ?>
 
-  <?php echo $JS_Info?>
+
+  <!-- Modal -->
+  <div class="modal fade" id="exampleModal" tabindex="-1" aria-labelledby="exampleModalLabel" aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered modal-dialog-scrollable">
+      <div class="modal-content">
+        <div class="modal-header">
+          <h5 class="modal-title" id="exampleModalLabel">Modal title</h5>
+          <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+        </div>
+        <div class="modal-body">
+
+          <div class="form-group row">
+            <label for="MainCategory_CD" class="col-md-3 col-form-label">大分類コード</label>
+            <input type="text" name="MainCategory_CD" id="MainCategory_CD" value="" class="form-control col-md-3" readonly>
+          </div>
+
+          <div class="form-group row">
+            <label for="MainCategory_Name" class="col-md-3 col-form-label">大分類名</label>
+            <input type="text" name="MainCategory_Name" id="MainCategory_Name" value="" class="form-control col-md-3">
+          </div>
+
+
+          <div class="modal-footer">
+            <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            <button type="button" class="btn btn-primary">Save changes</button>
+          </div>
+        </div>
+      </div>
+    </div>
+
+    <?php echo $JS_Info ?>
 </body>
 
 <script>
-  //画面起動時の処理
+  //画面遷移時
   $(window).on('load', function(event) {
     $("#btn_Insert").show();
     $("#btn_Update").hide();
     $("#btn_Delete").hide();
   });
 
-  //テーブルクリック時
-  $('.Table').on('click', function() {
-    var MainCategory_CD = $(this).children('td')[0].innerText;
-    $("#MainCategory_CD").val(MainCategory_CD);
 
-    var MainCategory_Name = $(this).children('td')[1].innerText;
-    $("#MainCategory_Name").val(MainCategory_Name);
 
-    var UsageFlag = $(this).children('td')[2].innerText;
+  $('#exampleModal').on('show.bs.modal', function(e) {
 
-    if (UsageFlag == '〇') {
-      $("#chk_UsageFlag").prop('checked', true);
-    } else {
-      $("#chk_UsageFlag").prop('checked', false);
-    }
-
-    $("#btn_Insert").hide();
-    document.getElementById("btn_Insert").disabled = true;
-    $("#btn_Update").show();
-    $("#btn_Delete").show();    
+    // イベント発生元
+    let evCon = $(e.relatedTarget);
+    var MainCategory_CD = evCon.data('MainCategory_CD');
+    var MainCategory_Name = evCon.data('MainCategory_Name');
+    $('#MainCategory_CD').val(MainCategory_CD);
+    $('#MainCategory_Name').val(MainCategory_Name);
 
   });
+
+  //テーブルクリック時
+  // $('.Table').on('click', function() {
+  //   var MainCategory_CD = $(this).children('td')[0].innerText;
+  //   $("#MainCategory_CD").val(MainCategory_CD);
+
+  //   var MainCategory_Name = $(this).children('td')[1].innerText;
+  //   $("#MainCategory_Name").val(MainCategory_Name);
+
+  //   var UsageFlag = $(this).children('td')[2].innerText;
+
+  //   if (UsageFlag == '〇') {
+  //     $("#chk_UsageFlag").prop('checked', true);
+  //   } else {
+  //     $("#chk_UsageFlag").prop('checked', false);
+  //   }
+
+  //   $("#btn_Insert").hide();
+  //   document.getElementById("btn_Insert").disabled = true;
+  //   $("#btn_Update").show();
+  //   $("#btn_Delete").show();
+
+  // });
 
 
   //登録ボタンクリック時
@@ -190,13 +232,12 @@
 
     if (!ErrorMsg == "") {
       ErrorMsg = '以下は必須項目です。\n' + ErrorMsg;
-      window.alert(ErrorMsg); 
+      window.alert(ErrorMsg);
       return false;
     } else {
       return true;
     }
   }
-  
 </script>
 
 </html>
